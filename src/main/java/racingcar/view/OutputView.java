71d41ea39c24459a6e8f1ca4883c15fad9acf439
jun <jwjjwj1960@naver.com>
2025-10-26 14:26:11 +0900

    fix(io): 우승자 출력 방식 오류 수정
    기존 ","에서 ", "로 오류 수정

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -5,13 +5,16 @@ import racingcar.model.Car;
 
 public class OutputView {
 
-    public void result(String format, List<String> output) {
-        System.out.printf(format, String.join(",", output));
+    // 최종 우승자 출력
+    public static void winner(String format, List<String> output) {
+        System.out.printf(format, String.join(", ", output));
     }
 
-    public static void process(List<Car> cars) {
+    // 실행 과정 출력
+    public static void raceProcess(List<Car> cars) {
         for (Car car : cars) {
             System.out.println(car.getName() + " : " + "-".repeat(car.getLocation()));
         }
+        System.out.println();
     }
 }
