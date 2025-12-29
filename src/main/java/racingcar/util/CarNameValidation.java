be1af663ff7e29ad33625483fe66b6def541266d
전원준 <jwjjwj1960@naver.com>
2025-12-29 18:40:28 +0900

    feat: CarNameValidation 추가
    자동차 이름, 중복여부, 영어이름, 공백 검증

---
+++ src/main/java/racingcar/util/CarNameValidation.java
@@ -0,0 +1,38 @@
+package racingcar.util;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class CarNameValidation {
+    private static final int CAR_LENGTH = 5;
+    private static final int MIN_TRYCOUNT = 1;
+    private static final int CAR_DUPLICATE = 1;
+
+    public static void validate(String carName) {
+        validateLength(carName);
+        validateBlank(carName);
+        validateFormat(carName);
+    }
+
+    private static void validateLength(String carName) {
+        if (carName.length() > CAR_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CARNAME_LENGTH.getMessage());
+        }
+    }
+
+    private static void validateBlank(String carName) {
+        if (carName.contains(" ")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BLANK.getMessage());
+        }
+    }
+
+    private static void validateFormat(String carName) {
+        if (Pattern.matches("^[a-zA-Z]", carName));
+    }
+
+    private static void validateDuplicate(List<String> cars) {
+        if (cars.size() != cars.stream().distinct().count()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_CARNAME_DUPLICATION.getMessage());
+        }
+    }
+}
