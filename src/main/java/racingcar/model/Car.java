39471f96ca99bdf68a02d33e42ee5fb0fe570fa2
전원준 <jwjjwj1960@naver.com>
2025-12-29 18:08:21 +0900

    feat: Car 객체 생성

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,13 @@
+package racingcar.model;
+
+import java.util.List;
+
+public class Car {
+    private final List<String> car;
+
+    public Car(List<String> car) {
+        this.car = car;
+    }
+
+
+}
