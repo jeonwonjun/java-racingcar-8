1e83e11e9f8d1d5457e4dbaa355bbc3f59b4a04c
전원준 <jwjjwj1960@naver.com>
2025-12-29 18:02:11 +0900

    feat: io 추가
    InputHandler, OutputHandler 생성

---
+++ src/main/java/racingcar/view/InputHandler.java
@@ -0,0 +1,10 @@
+package racingcar.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputHandler {
+
+    public String read() {
+        return readLine();
+    }
+}
---
+++ src/main/java/racingcar/view/OutputHandler.java
@@ -0,0 +1,8 @@
+package racingcar.view;
+
+public class OutputHandler {
+
+    public static void print() {
+        System.out.println();
+    }
+}
