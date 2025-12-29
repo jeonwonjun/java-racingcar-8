19f84ae73e2a845c0d3662044f2c8d2dd8b439b7
전원준 <jwjjwj1960@naver.com>
2025-12-29 18:20:49 +0900

    feat: ErrorMessage Enum 추가
    ErrorMessage를 Enum으로 관리,
    자동차 이름 및 시도횟수에 대한 에러메시지 정리
    자동차이름: 길이, 중복여부, 영어만 사용 가능, 빈칸여부
    시도횟수: 최소 범위, 숫자만 가능

---
+++ src/main/java/racingcar/util/ErrorMessage.java
@@ -0,0 +1,20 @@
+package racingcar.util;
+
+public enum ErrorMessage {
+    INVALID_CARNAME_LENGTH("자동차 이름은 5자 이하만 가능합니다."),
+    INVALID_CARNAME_DUPLICATION("자동차 이름은 중복될 수 없습니다."),
+    INVALID_CARNAME_FORMAT("자동차 이름에는 영어만 가능합니다."),
+    INVALID_BLANK("자동차 이름에는 공백이 있으면 안됩니다."),
+    INVALID_TRYCOUNT_ROUND("시도횟수는 최소 1번이어야 합니다."),
+    INVALID_TRYCOUNT_NUMBER_FORMAT("시도횟수는 숫자이어야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
