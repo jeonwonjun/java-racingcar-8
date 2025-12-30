47b4bbc5efb5f5a07922ca102f88fd8a8eeee2a0
전원준 <jwjjwj1960@naver.com>
2025-12-30 15:42:02 +0900

    feat: tryCount 검증 로직 추가
    TryCountValidation 생성해서
    시도횟수 최소 범위와 정수형이 아닐 경우 검증

--- src/main/java/racingcar/util/TryCountValidation.java
+++ src/main/java/racingcar/util/TryCountValidation.java
@@ -1,4 +1,19 @@
 package racingcar.util;
 
 public class TryCountValidation {
+    private static final int MIN_TRY_COUNT = 1;
+
+    public static void validateRange(int tryCount) {
+        if (tryCount < MIN_TRY_COUNT) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_TRYCOUNT_RANGE.getMessage());
+        }
+    }
+
+    public static int validateFormat(String tryCountInput) {
+        try {
+            return Integer.parseInt(tryCountInput);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_TRYCOUNT_NUMBER_FORMAT.getMessage());
+        }
+    }
 }
