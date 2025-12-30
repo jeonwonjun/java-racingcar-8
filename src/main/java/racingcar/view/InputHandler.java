aeb08385df40c6e9b0f9da1596c681fed48f6980
전원준 <jwjjwj1960@naver.com>
2025-12-30 15:42:59 +0900

    chore: 변경 x

--- src/main/java/racingcar/view/InputHandler.java
+++ src/main/java/racingcar/view/InputHandler.java
@@ -4,7 +4,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputHandler {
 
-    public String read() {
+    private static String read() {
         return readLine();
     }
 }
